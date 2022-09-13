e85881bd7dfb622915f5867955ddbaf41e16160b
Thomas <thomas.northey@pm.me>
2022-09-13 17:04:11 -0400

    nmm_modes files, animate not working...

--- animate_example.py
+++ animate_example.py
@@ -3,9 +3,9 @@ import molecule
 m = molecule.Molecule()
 nm = molecule.Normal_modes()
 
-xyz_start_file = 'xyz/furan.xyz'
-nmfile = 'nm/furan_normalmodes.txt'
-natoms = 9
+xyz_start_file = 'xyz/nmm.xyz'
+nmfile = 'nm/nmm_normalmodes.txt'
+natoms = 18
 modes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 , 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 for i in range(len(modes)):
     nm.animate_mode(modes[i], xyz_start_file, nmfile, natoms)
